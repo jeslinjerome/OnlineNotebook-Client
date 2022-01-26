e3e57ccebae2dec320a710b9cff5ee2ebd9e420d
Jeslin <jeslinjerome@gmail.com>
2022-01-26 17:23:22 +0530

    vd 59 uselocation hook yo toggle btw pages

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
   return (
-  <div>
-        This is Home
-  </div>
-  )
+    <div>
+      <div className="container my-3">
+        <h2>Add a Note</h2>
+        <form action="" className="my-3">
+          <div className="mb-3">
+            <lable className="form-lable">Add a Title</lable>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="mb-3">
+            <lable className="form-lable">Add a Note description</lable>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="mb-3">
+            <lable className="form-lable">Add a Tag</lable>
+            <input type="text" className="form-control" />
+          </div>
+        </form>
+      </div>
+
+      <div className="container my-3">
+        <h2>Your Notes</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
