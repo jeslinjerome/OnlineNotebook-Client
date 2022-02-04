a9488d8b74b01ec0155286492e4145938a605e68
Jeslin <jeslinjerome@gmail.com>
2022-02-04 20:40:02 +0530

    alerts are left

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Alert from './components/Alert';
 import NoteState from './context/notes/NoteState';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  // Switch,
-  Route,
-  // Link
-} from "react-router-dom";
 
 function App() {
   return (
@@ -19,13 +19,13 @@ function App() {
     <NoteState>
      <Router>
         <Navbar />
-        <Alert message="This is an amazing React Course"/>
+        <Alert message="on progress"/>
         <div className = "container">
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          {/* <Route path="/login" element={<Login />} /> */}
-          {/* <Route path="/signup" element={<Signup />} /> */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
         </div>
       </Router>
--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -11,12 +11,18 @@ const NoteItem = (props) => {
       <div className="col-md-3">
         <div className="card my-3">
             <div className="card-body">
-                <div className="d-flex align-item-center">
-                <h5 className="card-title">{note.title}</h5>
-                <i className="far fa-trash-alt mx-2" onClick={()=>{deleteNote(note._id)}}></i>
-                <i className="far fa-edit mx-2" onClick={()=>{updateNote(note)}}></i>
+                {/* <div className="d-flex align-item-center"> */}
+                <div className="d-flex justify-content-between">
+                  <div>
+                    <h5 className="card-title">{note.title}</h5>
+                  </div>
+                  <div>
+                  <i className="far fa-trash-alt mx-2" onClick={()=>{deleteNote(note._id)}}></i>
+                  <i className="far fa-edit mx-2" onClick={()=>{updateNote(note)}}></i>
+                  </div>
                 </div>
                 <p className="card-text">{note.description}</p>
+                <p className="card-text">#{note.tag}</p>
             </div>
         </div>
       </div>
